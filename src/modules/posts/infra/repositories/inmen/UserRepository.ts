593c1d26a30c54acd8245364bed216895af69631
David Marques <davidmarques99@hotmail.com>
2024-04-15 20:30:17 -0300

    finish repository

--- src/modules/posts/infra/repositories/inmen/UserRepository.ts
+++ src/modules/posts/infra/repositories/inmen/UserRepository.ts
@@ -1,10 +1,24 @@
 // Modules Imports
 import IUserRepository from '../../../repositories/IUserRepository';
+import { PrismaClient } from '@prisma/client';
 
 
-class UserRepository implements IUserRepository{
+const prisma = new PrismaClient();
+
+class User {
+	constructor(public name: string){}
+
+	async save(): Promise<void>{
+		await prisma.user.create({
+			data: {name: this.name}
+		});
+	}
+}
+
+class UserRepository implements IUserRepository {
 	async save(name: string): Promise<void> {
-		console.log(name);
+		const user = new User(name);
+		await user.save();
 	}
 }
 
